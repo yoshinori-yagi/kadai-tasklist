fd48734c11157f337198d62092cf674c6c1245e6
jiro.kirameki <ec2-user@ip-172-31-27-230.us-east-2.compute.internal>
2018-05-24 01:56:45 +0000

    tasklist-model

--- kadai-tasklist/database/migrations/2018_05_24_104602_create_tasks_table.php
+++ kadai-tasklist/database/migrations/2018_05_24_104602_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasklistTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTasklistTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasklist', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
             $table->string('content'); 
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateTasklistTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasklist');
+        Schema::dropIfExists('tasks');
     }
 }
