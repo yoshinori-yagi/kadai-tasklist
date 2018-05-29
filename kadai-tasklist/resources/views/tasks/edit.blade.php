5414cbf57ffb0bf79fd84d73c4dd2db6beca49f2
jiro.kirameki <ec2-user@ip-172-31-27-230.us-east-2.compute.internal>
2018-05-29 07:30:23 +0000

    bootstrap design

--- kadai-tasklist/resources/views/tasks/edit.blade.php
+++ kadai-tasklist/resources/views/tasks/edit.blade.php
@@ -5,7 +5,7 @@
 <h1>id: {{ $task->id }} のたすく編集ページ</h1>
 
   <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-6 col-sm-offset-2 col-sm-4 col-md-offset-2 col-md-4 col-lg-offset-3 col-lg-3">
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
                 <div class="form-group">
                     {!! Form::label('status', 'すてーたす:') !!}
--- kadai-tasklist/resources/views/tasks/index.blade.php
+++ kadai-tasklist/resources/views/tasks/index.blade.php
@@ -4,7 +4,7 @@
 
 <h1>たすく一覧</h1>
 
-     @if (count($messages) > 0)
+     @if (count($tasks) > 0)
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -14,7 +14,7 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach ($messages as $task)
+                @foreach ($tasks as $task)
                     <tr>
                         <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
                         <td>{{ $task->status }}</td>
